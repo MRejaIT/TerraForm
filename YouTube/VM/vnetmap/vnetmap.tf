a8668795db42cc54fec67dcff3e9a0fff4bbcda0
Md. Mohsin Reja <162022058+MRejaIT@users.noreply.github.com>
2024-10-14 01:05:54 +0600

    vnet map

---
+++ YouTube/VM/vnetmap/vnetmap.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+
+  subscription_id = "f432a24d-fc29-4e0a-9868-05f6e415e557"
+  #tenant_id       = "479e29ce-1a7a-4ee2-88c6-f4c3370ab5a1"
+  #client_id       = "0fd8a585-8613-46fd-8833-2d64838e4938"
+  #client_secret   = "OV_8Q~kttYWQhB2V7qd8C4w6LfNvDYKFMRje~aeV"
+}
+
+locals {
+  azurerm_resource_group = "TerraForm-RG-SEA-01"
+  location = "Southeast Asia"
+
+  vitrual_network = {
+    name = "VNet-01"
+    address_space = "10.0.0.0/16"
+  }
+
+    subnet=[
+        {
+            name="App-Subnet"
+            address_prefixes="10.0.1.0/24"
+        },
+
+        {
+            name="DB-Subnet"
+            address_prefixes="10.0.1.0/24"
+        }
+
+    ]
+}
+
+resource "azurerm_resource_group" "rgdetails" {
+  name     = local.azurerm_resource_group
+  location = local.location
+  
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = local.vitrual_network.name
+  location            = local.location
+  resource_group_name = local.azurerm_resource_group
+  address_space       = [local.vitrual_network.address_space]
+
+  depends_on = [ azurerm_resource_group.rgdetails ]
+}
+
+resource "azurerm_subnet" "appsubnet" {
+  name                 = local.subnet[0].name
+  resource_group_name  = local.azurerm_resource_group
+  virtual_network_name = local.vitrual_network.name
+  address_prefixes     = [local.subnet[0].address_prefixes]
+
+  depends_on = [ azurerm_resource_group.rgdetails ]
+}
+
+resource "azurerm_subnet" "dbsubnet" {
+  name                 = local.subnet[1].name
+  resource_group_name  = local.azurerm_resource_group
+  virtual_network_name = local.vitrual_network.name
+  address_prefixes     = [local.subnet[1].address_prefixes]
+
+  depends_on = [ azurerm_resource_group.rgdetails]
+}
