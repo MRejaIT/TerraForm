a187fae5796b8cc77f770603df8f417c177d391b
MRejaIT <mrejait@gmail.com>
2024-10-20 16:12:51 +0600

    nsg

--- YouTube/VM/vnetmap/vnetmap.tf
+++ YouTube/VM/vnetmap/vnetmap.tf
@@ -101,3 +101,28 @@ resource "azurerm_public_ip" "pip" {
   depends_on = [ azurerm_resource_group.rgdetails ]
 
 }
+
+resource "azurerm_network_security_group" "nsgdetails" {
+  name                = "AppNSG-01"
+  location            = local.location
+  resource_group_name = local.azurerm_resource_group
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  depends_on = [ azurerm_resource_group.rgdetails ]
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsglink" {
+  subnet_id                 = azurerm_subnet.appsubnet.id
+  network_security_group_id = azurerm_network_security_group.nsgdetails.id
+}
+
